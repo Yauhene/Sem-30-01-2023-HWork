55d32ab2e4ead30523e6464490449c3142655efe
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-01 21:27:16 +0300

    add Task10 is gone

---
+++ Task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.Write("Введите число, пожалуйста: ");
+//int singleDigit=0;
+
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    int singleDigit=0;
+    singleDigit=number;
+    singleDigit=singleDigit/10;
+    singleDigit=singleDigit%10;
+    Console.WriteLine("Второй цифрой с конца числа " + number + " является " + singleDigit);
+
+}
+
+else Console.WriteLine("Введенное значение не является целым числом");
+
+Console.WriteLine();
+Console.WriteLine();
